f1f0f568638f0c80e6d797dcbaabd5a234244603
Dhruva <dhruvatajapara@gmail.com>
2026-02-09 23:04:06 +0530

    cpp code

--- for.cpp
+++ for.cpp
@@ -6,6 +6,10 @@ int main(){
     for(int i = 0;i<=5;i++){
         cout << i << endl;
     }
-
+     for(int i = 1;i<=10;i++){
+        if(i % 2 == 0){
+            cout << i << endl;
+        }
+     }
     return 0;
 }
